73a19b84f95a1907091841eef6bad4c5db714a86
Albert Hopkins <marduk@letterboxes.org>
2023-12-10 11:07:55 -0600

    cli: add themability for the "Node" header

--- src/gbp_ps/cli/ps.py
+++ src/gbp_ps/cli/ps.py
@@ -64,7 +64,7 @@ def create_table(processes: ProcessList, args: argparse.Namespace) -> Table:
             f"[{phase}_phase]{phase:9}[/{phase}_phase]",
         ]
         if args.node:
-            row.append(process["buildHost"])
+            row.append(f"[buildHost]{process['buildHost']}[/buildHost]")
         table.add_row(*row)
 
     return table
