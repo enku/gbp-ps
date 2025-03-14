7f98972773f505032b36e0f2fa15c9d2068b5698
Albert Hopkins <marduk@letterboxes.org>
2025-03-13 19:13:42 -0500

    tests.__main__: add --failfast and other options
    To match my other gbp projects.

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Run tests for Gentoo Build Publisher"""
+import argparse
 import os
 import sys
 
@@ -10,7 +11,8 @@ from django.test.utils import get_runner
 
 def main() -> None:
     """Program entry point"""
-    os.environ["DJANGO_SETTINGS_MODULE"] = "gbp_testkit.settings"
+    args = parse_args()
+    os.environ["DJANGO_SETTINGS_MODULE"] = args.settings
 
     # These values are required in order to import the publisher module
     os.environ.setdefault("BUILD_PUBLISHER_JENKINS_BASE_URL", "http://jenkins.invalid/")
@@ -18,13 +20,24 @@ def main() -> None:
 
     django.setup()
 
-    tests = sys.argv[1:] or ["."]
-
     TestRunner = get_runner(settings)  # pylint: disable=invalid-name
-    test_runner = TestRunner(failfast=True, verbosity=2)
-    failures = test_runner.run_tests(tests)
+    verbosity = 2 if args.verbose else 1
+    test_runner = TestRunner(failfast=args.failfast, verbosity=verbosity)
+    failures = test_runner.run_tests(args.tests)
 
     sys.exit(bool(failures))
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse command-line arguments"""
+    default_settings = os.environ.get("DJANGO_SETTINGS_MODULE", "gbp_testkit.settings")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--failfast", action="store_true", default=False)
+    parser.add_argument("--settings", default=default_settings)
+    parser.add_argument("-v", "--verbose", action="store_true", default=False)
+    parser.add_argument("tests", nargs="*", default=["tests"])
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
