a55e815a9f6a42bdcec50a86e9c305c54d00483b
Albert Hopkins <marduk@letterboxes.org>
2025-03-15 14:40:04 -0500

    plugin: add version & description fields

--- src/gbp_ps/__init__.py
+++ src/gbp_ps/__init__.py
@@ -1,4 +1,14 @@
 """gbp-ps"""
 
+import importlib.metadata
+
+__version__ = importlib.metadata.version("gbp-ps")
+
 # Plugin definition
-plugin = {"name": "gbp-ps", "app": "gbp_ps", "graphql": "gbp_ps.graphql"}
+plugin = {
+    "name": "gbp-ps",
+    "version": __version__,
+    "description": "A plugin to display your Gentoo Build Publisher processes",
+    "app": "gbp_ps",
+    "graphql": "gbp_ps.graphql",
+}
