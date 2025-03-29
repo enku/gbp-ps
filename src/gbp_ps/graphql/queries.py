0325c68ae99117256f992a90df5094fe0f11988b
Albert Hopkins <marduk@letterboxes.org>
2025-03-29 18:43:02 -0500

    queries: buildProcesses: optimization tweaks

--- src/gbp_ps/graphql/queries.py
+++ src/gbp_ps/graphql/queries.py
@@ -1,6 +1,6 @@
 """Query GraphQL resolver for gbp-ps"""
 
-from typing import Any, TypeAlias
+from typing import Any, Iterable, TypeAlias
 
 from ariadne import ObjectType, convert_kwargs_to_snake_case
 from graphql import GraphQLResolveInfo
@@ -13,18 +13,17 @@ Info: TypeAlias = GraphQLResolveInfo
 Query = ObjectType("Query")
 
 
+get_processes = Repo(Settings.from_environ()).get_processes
+
+
 @Query.field("buildProcesses")
 @convert_kwargs_to_snake_case
 def _(
     _obj: Any, _info: Info, *, include_final: bool = False, machine: str
-) -> list[BuildProcess]:
+) -> Iterable[BuildProcess]:
     """Return the list of BuildProcesses
 
     If include_final is True also include processes in their "final" phase. The default
     value is False.
     """
-    return list(
-        Repo(Settings.from_environ()).get_processes(
-            include_final=include_final, machine=machine
-        )
-    )
+    return get_processes(include_final=include_final, machine=machine)
