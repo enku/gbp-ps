89cec2a3389d9ccbfab93d6133d5fd9ab7cae011
Albert Hopkins <marduk@letterboxes.org>
2025-09-08 09:54:55 -0500

    tests: use @params instead of @parametrized

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,30 +1,28 @@
 """Tests for the exceptions module"""
 
 # pylint: disable=missing-docstring
-from typing import Any
 from unittest import TestCase, mock
 
-from unittest_fixtures import parametrized
+from unittest_fixtures import Fixtures, params
 
 from gbp_ps.exceptions import RETURN_EXCEPTION, swallow_exception
 
 
+@params(returns=[None, 6, RETURN_EXCEPTION])
+@params(expected=["test", 6, "test"])
+@params(side_effect=[None, Exception, Exception])
 class SwallowExceptionTests(TestCase):
-    @parametrized(
-        [(None, "test", None), (6, 6, Exception), (RETURN_EXCEPTION, "test", Exception)]
-    )
-    def test_swallow_exception(
-        self, returns: Any, expected: Any, side_effect: type[BaseException]
-    ) -> None:
-
-        with mock.patch.object(self, "func", wraps=self.func, side_effect=side_effect):
-            wrapped = swallow_exception(Exception, returns=returns)(self.func)
+    def test_swallow_exception(self, fixtures: Fixtures) -> None:
+        with mock.patch.object(
+            self, "func", wraps=self.func, side_effect=fixtures.side_effect
+        ):
+            wrapped = swallow_exception(Exception, returns=fixtures.returns)(self.func)
             result = wrapped()
 
-        if returns is RETURN_EXCEPTION:
-            self.assertIsInstance(result, side_effect)
+        if fixtures.returns is RETURN_EXCEPTION:
+            self.assertIsInstance(result, fixtures.side_effect)
         else:
-            self.assertEqual(result, expected)
+            self.assertEqual(result, fixtures.expected)
 
     @staticmethod
     def func() -> str:
