7f37ddc286c581a2af693ba53c9553b6e2d73022
Albert Hopkins <marduk@letterboxes.org>
2025-09-21 17:38:09 -0500

    web ui: do more efficient DOM updates
    Only replace what needs replacing instead of doing a "full screen
    refresh" on each cycle.

--- src/gbp_ps/django/gbp_ps/static/gbp_ps/ps.js
+++ src/gbp_ps/django/gbp_ps/static/gbp_ps/ps.js
@@ -51,35 +51,71 @@ function elapsed(timeString, now) {
   ].join(':');
 }
 
+function getRowId(process) {
+  const sanitizedPackage = process.package.replace(/\//g, '_');
+
+  return `${process.machine}_${process.id}_${sanitizedPackage}`;
+}
+
+function removeGoneProcesses(processes) {
+  const rowIds = Array.from(processes, (process) => getRowId(process));
+
+  tbody.querySelectorAll('tr').forEach((row) => {
+    if (rowIds.indexOf(row.id) < 0) {
+      tbody.removeChild(row);
+    }
+  });
+}
+
 function setProcesses(processes, now) {
-  const rows = processes.map((process) => {
+  removeGoneProcesses(processes);
+
+  processes.map((process) => {
     const { phase } = process;
-    const tr = document.createElement('tr');
-    const index = buildPhases.indexOf(phase);
     let progressWidth = 100;
     let progressClass = 'progress-bar progress-bar-striped progress-bar-animated';
     let progressColor = gradientColors[0];
+    const index = buildPhases.indexOf(phase);
 
     if (index >= 0) {
       progressWidth = Math.floor(((index + 1) / buildPhases.length) * 100);
       progressColor = colorMap[phase];
       progressClass = 'progress-bar';
     }
+    const progressBar = `
+            <div class="${progressClass}" role="progressbar" style="background-color: ${progressColor}; width: ${progressWidth}%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="${buildPhases.length}"></div>`;
+    const rowId = getRowId(process);
+    const elapseTime = elapsed(process.startTime, now);
+    let tr = document.getElementById(rowId);
+
+    if (tr) {
+      tr.querySelector('td.elapsed').textContent = elapseTime;
+      const phaseElem = tr.querySelector('td.phase');
+
+      if (phaseElem.textContent !== phase) {
+        phaseElem.textContent = process.phase;
+        const progress = tr.querySelector('div.progress');
+        progress.innerHTML = progressBar;
+      }
+      return phase;
+    }
+    tr = document.createElement('tr');
+    tr.id = rowId;
     tr.innerHTML = `
       <td>${process.machine}</td>
       <td class="numeric">${process.id}</td>
-      <td class="package">${process.package}</td>
-      <td class="numeric">${elapsed(process.startTime, now)}</td>
-      <td class="phase">${phase}</td>
-      <td class="phase-progress">
-        <div class="progress">
-            <div class="${progressClass}" role="progressbar" style="background-color: ${progressColor}; width: ${progressWidth}%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="${buildPhases.length}"></div>
-        </div>
-      </td>
-    `;
-    return tr;
+       <td class="package">${process.package}</td>
+       <td class="elapsed numeric">${elapseTime}</td>
+       <td class="phase">${phase}</td>
+       <td class="phase-progress">
+         <div class="progress">
+           ${progressBar}
+         </div>
+       </td>
+     `;
+    tbody.appendChild(tr);
+    return phase;
   });
-  tbody.replaceChildren(...rows);
 }
 
 function getInterval() {
